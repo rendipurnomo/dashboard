e64a5f1ed1fea3ad58179f033536b775f8907a64
rendi <Rendipur234@gmail.com>
2023-04-04 04:01:39 +0700

    sidebar menu

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import { Navbar, Footer,Sidebar, ThemeSetting } from './components';
 import { Ecommerce,Orders,Calender,Employees,Stacked,Pyramid,Customers,Kanban,Area,Bar,Pie, Line,Financial,ColorPicker,ColorMapping, Editor } from './pages';
 
+import { useStateContext } from './contexts/ContextProvider';
+
 import "./App.css"
 
 const App = () => {
-const activeMenu = true;
+const {activeMenu} = useStateContext();
 
   return (
     <>
